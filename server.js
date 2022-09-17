60fabcd970368f5defc5da0ad3191a83a62104da
Aryan Pareshbhai Patel <aryanpatel2602@gmail.com>
2022-09-16 21:29:45 -0400

    Added Comments.

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Aryan Pareshbhai Patel Student ID: 133889212 Date: 16/09/2022
+*
+*  Online (Cyclic) URL: https://jade-rich-coral.cyclic.app/ 
+*
+********************************************************************************/
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
